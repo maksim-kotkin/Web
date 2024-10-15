ad62e63ea090b1baf6dd214c58fc53cca5512255
Maksim_Kotkin <kotkin.maksim.02@mail.ru>
2024-10-15 18:07:57 +0300

    New commit

---
+++ 1.2-deploy/2.1-databases/models_list_displaying/books/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from books.models import Book
+from django.core.paginator import Paginator
+
+def books_view(request):
+    template = 'books/books_list.html'
+    list_book = Book.objects.all()
+    context = {'list_book': list_book}
+    return render(request, template, context)
+
+def book_date(request, pub_date):
+    template = 'books/books_list.html'
+    books_objects = Book.objects.filter(pub_date=pub_date)
+    books_next = (Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first())
+    if books_next:
+        books_next = str(books_next.pub_date)
+    # books_next = str(Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first().pub_date)
+    else:
+        books_next = None
+    books_previous = Book.objects.filter(pub_date__lt=pub_date).order_by('pub_date').first()
+    if books_previous:
+        books_previous = str(books_previous.pub_date)
+    else:
+        books_previous = None
+        # books_previous = str(Book.objects.filter(pub_date__lt=pub_date).order_by('pub_date').first().pub_date)
+    context = {
+        'books': books_objects,
+        'next_book': books_next,
+        'previous_book': books_previous,
+    }
+    return render(request, template, context)
