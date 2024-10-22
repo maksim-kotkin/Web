a3307f08b3c3b617e447e52c3d2d6cad0e5bb879
maksim-kotkin <kotkin.maksim.02@mail.ru>
2024-10-22 23:58:53 +0300

    Update project.yml

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,4 @@ git pull origin new
 source 1.5-ci-cd/work_with_database/env/bin/activate
 pip install -r 1.5-ci-cd/work_with_database/requirements.txt
 python manage.py migrate
-<<<<<<< HEAD
 sudo systemctl restart gunicorn
-=======
-sudo systemctl restart gunicorn
->>>>>>> 7357f0d (Update project.yml)
