e7caed28d04ac3be4d42e53cb43f5246d4426171
maksim-kotkin <129731484+maksim-kotkin@users.noreply.github.com>
2024-10-22 23:26:30 +0300

    Update project.yml

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/kotkinmax/prj/Web/1.5-ci-cd/work_with_database
+cd 1.5-ci-cd/work_with_database
 git pull origin new
-source /home/kotkinmax/prj/Web/1.5-ci-cd/work_with_database/env/bin/activate
-pip install -r /home/kotkinmax/prj/Web/1.5-ci-cd/work_with_database/requirements.txt
+source 1.5-ci-cd/work_with_database/env/bin/activate
+pip install -r 1.5-ci-cd/work_with_database/requirements.txt
 python manage.py migrate
 sudo systemctl restart gunicorn
